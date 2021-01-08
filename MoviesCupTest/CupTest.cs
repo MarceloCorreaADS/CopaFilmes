9d5ca04dbc81e7f75c662dec86bfe9f56885786e
Marcelo Correa <marcelocorreaads@gmail.com>
2021-01-08 17:14:54 -0300

    Finalizado criação de testes do Model Cup, correçoes no metodo CupResult

---
+++ MoviesCupTest/CupTest.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using backend.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MoviesCupTest
+{
+    [TestClass]
+    public class CupTest
+    {
+        // MoviesCup = Preencher com os filmes que vão participar da Copa Filmes
+        // ReturnExpected = Preencher com os filmes corretos que devem retornar
+        [TestMethod]
+        public void OrdenationMoviesIsCorrect()
+        {
+            List<Movie> MoviesCup = new List<Movie>();
+            List<Movie> ReturnExpected = new List<Movie>();
+
+            MoviesCup.Add(new Movie("tt3606756", "Os Incríveis 2", 2018, 8.5));
+            MoviesCup.Add(new Movie("tt4881806", "Jurassic World: Reino Ameaçado", 2018, 6.7));
+            MoviesCup.Add(new Movie("tt5164214", "Oito Mulheres e um Segredo", 2018, 6.3));
+            MoviesCup.Add(new Movie("tt7784604", "Hereditário", 2018, 7.8));
+            MoviesCup.Add(new Movie("tt4154756", "Vingadores: Guerra Infinita", 2018, 8.8));
+            MoviesCup.Add(new Movie("tt5463162", "Deadpool 2", 2018, 8.1));
+            MoviesCup.Add(new Movie("tt3778644", "Han Solo: Uma História Star Wars", 2018, 7.2));
+            MoviesCup.Add(new Movie("tt3501632", "Thor: Ragnarok", 2017, 7.9));
+
+            ReturnExpected.Add(new Movie("tt5463162", "Deadpool 2", 2018, 8.1));
+            ReturnExpected.Add(new Movie("tt3778644", "Han Solo: Uma História Star Wars", 2018, 7.2));
+            ReturnExpected.Add(new Movie("tt7784604", "Hereditário", 2018, 7.8));
+            ReturnExpected.Add(new Movie("tt4881806", "Jurassic World: Reino Ameaçado", 2018, 6.7));
+            ReturnExpected.Add(new Movie("tt5164214", "Oito Mulheres e um Segredo", 2018, 6.3));
+            ReturnExpected.Add(new Movie("tt3606756", "Os Incríveis 2", 2018, 8.5));
+            ReturnExpected.Add(new Movie("tt3501632", "Thor: Ragnarok", 2017, 7.9));
+            ReturnExpected.Add(new Movie("tt4154756", "Vingadores: Guerra Infinita", 2018, 8.8));
+
+            Cup CupMovies = new Cup(MoviesCup);
+
+            List<Movie> CupMoviesOrdination = CupMovies.CupOrdination;
+
+            CollectionAssert.AreEqual(ReturnExpected, CupMoviesOrdination, new MovieListComparer(), "Copa ordenada incorretamente!");
+        }
+
+        [TestMethod]
+        public void SemifinalsResultsIsCorrect()
+        {
+            List<Movie> MoviesCup = new List<Movie>();
+            List<Movie> ReturnExpected = new List<Movie>();
+
+            MoviesCup.Add(new Movie("tt3606756", "Os Incríveis 2", 2018, 8.5));
+            MoviesCup.Add(new Movie("tt4881806", "Jurassic World: Reino Ameaçado", 2018, 6.7));
+            MoviesCup.Add(new Movie("tt5164214", "Oito Mulheres e um Segredo", 2018, 6.3));
+            MoviesCup.Add(new Movie("tt7784604", "Hereditário", 2018, 7.8));
+            MoviesCup.Add(new Movie("tt4154756", "Vingadores: Guerra Infinita", 2018, 8.8));
+            MoviesCup.Add(new Movie("tt5463162", "Deadpool 2", 2018, 8.1));
+            MoviesCup.Add(new Movie("tt3778644", "Han Solo: Uma História Star Wars", 2018, 7.2));
+            MoviesCup.Add(new Movie("tt3501632", "Thor: Ragnarok", 2017, 7.9));
+
+
+            ReturnExpected.Add(new Movie("tt4154756", "Vingadores: Guerra Infinita", 2018, 8.8));
+            ReturnExpected.Add(new Movie("tt3501632", "Thor: Ragnarok", 2017, 7.9));
+            ReturnExpected.Add(new Movie("tt3606756", "Os Incríveis 2", 2018, 8.5));
+            ReturnExpected.Add(new Movie("tt4881806", "Jurassic World: Reino Ameaçado", 2018, 6.7));
+          
+            Cup CupMovies = new Cup(MoviesCup);
+
+            CupMovies.CupResult();
+
+            List<Movie> CupMoviesSemifinals = CupMovies.Semifinals;
+
+            CollectionAssert.AreEqual(ReturnExpected, CupMoviesSemifinals, new MovieListComparer(), "Partidas semifnais incorretas!");
+        }
+
+        [TestMethod]
+        public void finalsResultsIsCorrect()
+        {
+            List<Movie> MoviesCup = new List<Movie>();
+            List<Movie> ReturnExpected = new List<Movie>();
+
+            MoviesCup.Add(new Movie("tt3606756", "Os Incríveis 2", 2018, 8.5));
+            MoviesCup.Add(new Movie("tt4881806", "Jurassic World: Reino Ameaçado", 2018, 6.7));
+            MoviesCup.Add(new Movie("tt5164214", "Oito Mulheres e um Segredo", 2018, 6.3));
+            MoviesCup.Add(new Movie("tt7784604", "Hereditário", 2018, 7.8));
+            MoviesCup.Add(new Movie("tt4154756", "Vingadores: Guerra Infinita", 2018, 8.8));
+            MoviesCup.Add(new Movie("tt5463162", "Deadpool 2", 2018, 8.1));
+            MoviesCup.Add(new Movie("tt3778644", "Han Solo: Uma História Star Wars", 2018, 7.2));
+            MoviesCup.Add(new Movie("tt3501632", "Thor: Ragnarok", 2017, 7.9));
+
+            ReturnExpected.Add(new Movie("tt4154756", "Vingadores: Guerra Infinita", 2018, 8.8));
+            ReturnExpected.Add(new Movie("tt3606756", "Os Incríveis 2", 2018, 8.5));
+
+
+            Cup CupMovies = new Cup(MoviesCup);
+
+            CupMovies.CupResult();
+
+            List<Movie> CupMoviesfinals = CupMovies.Finals;
+
+            CollectionAssert.AreEqual(ReturnExpected, CupMoviesfinals, new MovieListComparer(), "Partida final incorreta!");
+        }
+
+        [TestMethod]
+        public void ReturnMovieListWith1stAnd2stPlaces()
+        {
+            List<Movie> MoviesCup = new List<Movie>();
+            List<Movie> ReturnExpected = new List<Movie>();
+
+            MoviesCup.Add(new Movie("tt3606756", "Os Incríveis 2", 2018, 8.5));
+            MoviesCup.Add(new Movie("tt4881806","Jurassic World: Reino Ameaçado",2018,6.7));
+            MoviesCup.Add(new Movie("tt5164214","Oito Mulheres e um Segredo",2018,6.3));
+            MoviesCup.Add(new Movie("tt7784604","Hereditário",2018,7.8));
+            MoviesCup.Add(new Movie("tt4154756","Vingadores: Guerra Infinita",2018,8.8));
+            MoviesCup.Add(new Movie("tt5463162","Deadpool 2",2018,8.1));
+            MoviesCup.Add(new Movie("tt3778644","Han Solo: Uma História Star Wars",2018,7.2));
+            MoviesCup.Add(new Movie("tt3501632","Thor: Ragnarok",2017,7.9));
+
+            ReturnExpected.Add(new Movie("tt4154756", "Vingadores: Guerra Infinita",2018,8.8));
+            ReturnExpected.Add(new Movie("tt3606756", "Os Incríveis 22",2018,8.5));
+
+            Cup CupMovies = new Cup(MoviesCup);
+
+            List<Movie> ResultCup = CupMovies.CupResult();
+
+            CollectionAssert.AreEqual(ReturnExpected, ResultCup, new MovieListComparer(), "Lista Final incorreta");
+            
+        }
+
+        private class MovieListComparer : Comparer<Movie>
+        {
+            public override int Compare([AllowNull] Movie x, [AllowNull] Movie y)
+            {
+
+                int i = x.Id.CompareTo(y.Id);
+
+                return i;
+            }
+        }
+    }
+}
--- backend/Models/Cup.cs
+++ backend/Models/Cup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,11 @@ namespace backend.Models
     public class Cup
     {
         public List<Movie> Movies;
+        public Movie Winner;
+        public Movie SecondPlace;
+        public List<Movie> CupOrdination;
+        public List<Movie> Semifinals;
+        public List<Movie> Finals;
 
         private Cup()
         {
@@ -17,40 +23,64 @@ namespace backend.Models
         public Cup(List<Movie> cupMovies)
         {
             // Recebendo a lista de filmes participantes ja à ordeno em ordem alfabética.
-            Movies = cupMovies.OrderBy(m => m.Titulo.Trim().Substring(m.Titulo.Trim().IndexOf(" "))).ToList();
+            Movies = cupMovies;
+            CupOrdination = Movies.OrderBy(m => m.Titulo.Trim()).ToList();
+            Semifinals = new List<Movie>();
+            Finals = new List<Movie>();
+
         }
 
         public List<Movie> CupResult()
         {
+            //Variaveis auxiliares
             Match match;
             Movie matchWinner;
-            List<Movie> Semifinals = new List<Movie>();
-            List<Movie> final = new List<Movie>();
+            List<Movie> finalsAux = new List<Movie>();
             List<Movie> Ranked = new List<Movie>();
 
-            int j = Movies.Count();
-            for (int i = 0; i < (Movies.Count()/2); i++, j--)
+            int CupRange = CupOrdination.Count() - 1;
+
+            //Verifico o tamanho da lista
+            int j = CupRange;
+            //For com metade da lista, pois metade da lista enfrentará a outra metade
+            // j-- usado para definir o indice ultimo da lista e irá diminuindo
+            for (int i = 0; i <= (CupRange/2); i++, j--)
             {
-                match = new Match(Movies[i], Movies[j]);
+                //definição da partida - primeiro enfrenta o ultimo, segundo o penultimo e assim por diante
+                match = new Match(CupOrdination[i], CupOrdination[j]);
                 matchWinner = match.MatchWinner();
+                // lista com os Filmes da semifinal
                 Semifinals.Add(matchWinner);
             }
 
-            for(int i = 0; i < (Semifinals.Count()/2); i += 2)
+            for(int i = 0; i <= ((Semifinals.Count())/2); i += 2)
             {
-                match = new Match(Movies[i], Movies[i+1]);
+                match = new Match(Semifinals[i], Semifinals[i+1]);
                 matchWinner = match.MatchWinner();
-                final.Add(matchWinner);
+                Finals.Add(matchWinner);
             }
 
-            match = new Match(final[0], final[1]);
-            matchWinner = match.MatchWinner();
+            //Partida final
+            match = new Match(Finals[0], Finals[1]);
+            //Guardo o vencedor na propriedade da classe
+            Winner = match.MatchWinner();
+            //Adiciono o vencedor na lista de retorno primeiro para ser o indice 0 da lista
+            Ranked.Add(Winner);
 
-            final.Remove(matchWinner);
-            Ranked.Add(matchWinner);
+            //Usado variavel auxiliar para não perder ordenação da Final
+            foreach(Movie m in Finals)
+            {
+                finalsAux.Add(m);
+            }
+            //Removo o vencedor para saber quem é o segundo lugar
+            finalsAux.Remove(Winner);
+            //Salvo o segundo lugar na propriedade da classe
+            SecondPlace = finalsAux[0];
 
-            Ranked.Add(final[0]);
+            //adiciono o segundo lugar na lista de retorno 
+            Ranked.Add(SecondPlace);
 
+            //Ranked[0] = Winner / Ranked[1] = SecondPlace
             return Ranked;
         }
     }
