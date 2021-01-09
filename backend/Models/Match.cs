69f183bcb7ad7a5e088b92d8d55355ca666524e8
Marcelo Correa <marcelocorreaads@gmail.com>
2021-01-09 17:08:23 -0300

    Correção no model Match, adicionado ordenação alfabetica

--- backend/Models/Match.cs
+++ backend/Models/Match.cs
@@ -19,15 +19,20 @@ namespace backend.Models
         public Match(Movie Movie1stMatch, Movie Movie2stMatch)
         {
             Movie1st = Movie1stMatch;
-            Movie2st = Movie2stMatch;
+            Movie2st = Movie2stMatch;            
         }
 
         public Movie MatchWinner()
         {
+            List<Movie> UnorderedMovies = new List<Movie>();
+            UnorderedMovies.Add(Movie1st);
+            UnorderedMovies.Add(Movie2st);
+
             // Regra de empate: Definido pela ordem alfabética.
-            // Filmes já estão em ordem alfabética.
+            // Filmes perdem a ordenação por causa das chaves, por isso reoordeno antes de realizar a disputa
+            List<Movie> orderedMovies = UnorderedMovies.OrderBy(m => m.Titulo.Trim()).ToList();
             // Se a nota do primeiro filme for maior ou igual a do segundo ele vence.
-            Movie winner = Movie1st.Nota >= Movie2st.Nota ? Movie1st : Movie2st;
+            Movie winner = orderedMovies[0].Nota >= orderedMovies[1].Nota ? orderedMovies[0] : orderedMovies[1];
 
             return winner;
         }
