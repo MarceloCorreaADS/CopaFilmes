76e6f464d799c6a1bf29f02754c4f378c0831a41
Marcelo Correa <marcelocorreaads@gmail.com>
2021-01-11 01:46:49 -0300

    Correções no test cupResult

--- MoviesCupTest/CupTest.cs
+++ MoviesCupTest/CupTest.cs
@@ -78,7 +78,7 @@ namespace MoviesCupTest
             List<Movie> MoviesCup = new List<Movie>();
             List<Movie> ReturnExpected = new List<Movie>();
 
-            MoviesCup.Add(new Movie("tt3606756", "Os Incríveis 2", 2018, 8.5));
+            MoviesCup.Add(new Movie("tt3606756", "Os Incríveis 2", 2018, 8.8));
             MoviesCup.Add(new Movie("tt4881806", "Jurassic World: Reino Ameaçado", 2018, 6.7));
             MoviesCup.Add(new Movie("tt5164214", "Oito Mulheres e um Segredo", 2018, 6.3));
             MoviesCup.Add(new Movie("tt7784604", "Hereditário", 2018, 7.8));
@@ -87,8 +87,10 @@ namespace MoviesCupTest
             MoviesCup.Add(new Movie("tt3778644", "Han Solo: Uma História Star Wars", 2018, 7.2));
             MoviesCup.Add(new Movie("tt3501632", "Thor: Ragnarok", 2017, 7.9));
 
+            
             ReturnExpected.Add(new Movie("tt4154756", "Vingadores: Guerra Infinita", 2018, 8.8));
-            ReturnExpected.Add(new Movie("tt3606756", "Os Incríveis 2", 2018, 8.5));
+            ReturnExpected.Add(new Movie("tt3606756", "Os Incríveis 2", 2018, 8.8));
+
 
 
             Cup CupMovies = new Cup(MoviesCup);
@@ -115,8 +117,10 @@ namespace MoviesCupTest
             MoviesCup.Add(new Movie("tt3778644","Han Solo: Uma História Star Wars",2018,7.2));
             MoviesCup.Add(new Movie("tt3501632","Thor: Ragnarok",2017,7.9));
 
+            
             ReturnExpected.Add(new Movie("tt4154756", "Vingadores: Guerra Infinita",2018,8.8));
-            ReturnExpected.Add(new Movie("tt3606756", "Os Incríveis 22",2018,8.5));
+            ReturnExpected.Add(new Movie("tt3606756", "Os Incríveis 2", 2018, 8.8));
+
 
             Cup CupMovies = new Cup(MoviesCup);
 
